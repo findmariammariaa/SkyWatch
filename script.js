99db15490dc643cdf6241c38c306ce3eadae3100
Mariam Maria <mariammaria38355@gmail.com>
2024-09-20 19:58:41 +0600

    Added javascript code

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const userLocation = document.getElementById("userLocation"),
+
+    converter= document.getElementById("converter"),
+    weatherIcon = document.querySelector(".weatherIcon"),
+    temperature = document.querySelector(".temperature"),
+    feelsLike = document.querySelector(".feelsLike"),
+    description =document.querySelector(".description"),
+    date=document.querySelector(".date"),
+    city =document.querySelector(".city"),
+
+    HValue= document.getElementById("HValue"),
+    WValue=document.getElementById("WValue"),
+    SRValue=document.getElementById("SRValue"),
+    SSValue=document.getElementById("SSValue"),
+    CValue= document.getElementById("CValue"),
+    UVValue=document.getElementById("UVValue"),
+    PValue=document.getElementById("PValue"),
+
+    Forecast =document.querySelector(".Forecast");
+
+
+// Listen for the "Enter" key on the input field
+userLocation.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent the default behavior of form submission
+        findUserLocation(); // Trigger the search
+    }
+});
+const WEATHER_API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=47fdaa370be6671be187850a1dc5ea87&q=`;
+const WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/2.5/onecall?appid=47fdaa370be6671be187850a1dc5ea87&exclude=minutely&units=metric`;
+
+function findUserLocation(){
+    fetch(WEATHER_API_ENDPOINT+ userLocation.value)
+    .then((response)=> response.json())
+    .then((data)=>{
+        if(data.cod!="" && data.cod!=200){
+            alert(data.message);
+            return;
+        }
+        console.log(data);
+        city.innerHTML = data.name + "," + data.sys.country;
+        weatherIcon.style.background = `url(https://openweathermap.org/img/wn/${data.weather[0]}10d@2x.png)` 
+        fetch(
+            WEATHER_DATA_ENDPOINT + `lon=${data.coord.lon}&lat=${data.coord.lat}`
+        )
+        .then((response)=> response.json())
+        .then((data)=>{
+            console.log(data);
+        });
+    });
+}
